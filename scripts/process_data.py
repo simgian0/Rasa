9f41f93f4b3ea4526c5566805b1476e91f8ce3e2
simgian0 <simonegiano@hotmail.com>
2025-02-14 11:40:11 +0100

    last train

--- scripts/process_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Carica il dataset
-df = pd.read_csv('/home/arthas0130/Rasa/Dataset DnD/over_one_mil_chars.csv')
-
-# Elimina i duplicati e visualizza le classi uniche
-unique_classes = df['class_starting'].dropna().unique().tolist()
-
-print(unique_classes)
